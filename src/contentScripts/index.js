0e16645eb2224da442205d22c89bb6695eb82f39
minung.han <hmu332233@gmail.com>
2019-07-03 02:07:40 +0900

    build: add hotkeys-js

--- src/contentScripts/index.js
+++ src/contentScripts/index.js
@@ -1,9 +1,18 @@
-const { getDomPath } = require('./modules/dom');
+import { getDomPath } from './modules/dom';
+import hotkeys from 'hotkeys-js';
+ 
+let domPath = '';
 
 document.addEventListener(
   'click',
   e => {
-    console.log(getDomPath(e.target));
+    domPath = getDomPath(e.target);
+    console.log(domPath);
   },
   false
 );
+
+hotkeys('ctrl+alt+k', function(event, handler){
+  event.preventDefault();
+  document.querySelector(domPath).click();
+});
