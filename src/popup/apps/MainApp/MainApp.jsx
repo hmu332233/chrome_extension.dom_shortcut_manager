22284c37cd914d3ea9890424fe6df27729353b82
minung.han <hmu332233@gmail.com>
2019-07-04 01:39:00 +0900

    build: add react development env

---
+++ src/popup/apps/MainApp/MainApp.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './MainApp.scss';
+
+function MainApp(props) {
+  return (
+    <div className={styles.MainApp}>
+      test
+    </div>
+  );
+}
+
+MainApp.propTypes = {
+};
+MainApp.defaultProps = {
+};
+
+export default MainApp;
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,38 @@
-const webpack = require('webpack');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
-    contentScripts: './src/contentScripts/index.js'
+    contentScripts: './src/contentScripts/index.js',
+    popup: './src/popup/index.js',
   },
   output: {
     path: `${ __dirname}/build`,
     filename: '[name].js',
-    publicPath: './'
+    publicPath: './',
   },
-  module: {},
-  plugins: [],
-  optimization: {},
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ["css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    // new MiniCssExtractPlugin({
+    //   filename: 'style.css'
+    // })
+  ],
+  optimization: {
+    minimize: true
+  },
   resolve: {
     modules: ['node_modules'],
-    extensions: ['.js', '.json', '.css']
+    extensions: ['.jsx', '.js', '.scss']
   }
 };
